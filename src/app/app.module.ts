45e097f2a43390e76c867f9dca403f77c157917c
s3rtr3s <pepemiguelx2@gmail.com>
2023-04-17 22:31:28 +0200

    no funca

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    SharedModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styles: [],
+})
+export class MenuComponent {
+  public menuItems: MenuItem[] = [];
+
+  ngOnInit() {
+    this.menuItems = [
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-fw pi-desktop',
+        items: [
+          {
+            label: 'Textos y Fechas',
+            icon: 'pi pi-fw pi-align-left',
+            routerLink: '/',
+          },
+          {
+            label: 'Números',
+            icon: 'pi pi-fw pi-dollar',
+            routerLink: 'numbers',
+          },
+          {
+            label: 'No comunes',
+            icon: 'pi pi-fw pi-globe',
+            routerLink: 'uncommon',
+          },
+        ],
+      },
+      {
+        label: 'Pipes Personalizados',
+        icon: 'pi pi-fw pi-cog',
+        items: [
+          {
+            label: 'Otro elemento',
+            icon: 'pi pi-fw pi-cog',
+          },
+        ],
+      },
+    ];
+  }
+}
